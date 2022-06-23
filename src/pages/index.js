f8b50effacd851cc3f66e3e8eb7a63d07ac4637f
danawanb <danawanbimantoro23@gmail.com>
2022-06-23 14:54:46 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/doc1')}>
+              to={useBaseUrl('docs/dasar1')}>
               Baca Buku Saku 📰
             </Link>
           </div>
