fe64ff0a9a081e8fa6d0a4ff28471027ee9f5596
danawanb <danawanbimantoro23@gmail.com>
2022-06-23 16:58:00 +0700

    Create todo.js

---
+++ src/pages/todo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Jadwal() {
+  return (
+    <Layout title="Todo" description="aaaaaaaaaaaaaaaaaaaaaaaaa">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+         aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </p>
+      </div>
+    </Layout>
+  );
+}
