b42b1239a4fb31398b41a1032dda41fa47cfecea
danawanb <danawanbimantoro23@gmail.com>
2022-06-23 22:41:05 +0700

    Update todo.js

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
+
 
 export default function Jadwal() {
   return (
@@ -17,26 +16,6 @@ export default function Jadwal() {
         <p>
           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         </p>
-
-        <Tabs className="unique-tabs">
-          <TabItem value="冬が来た">aaaaaaaaaaaa</TabItem>
-          <TabItem value="白い樹樹の光を">aaaaaaaaaa</TabItem>
-          <TabItem value="adfasf">aaaaa</TabItem>
-        </Tabs>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <Tabs className="unique-tabs">
-          <TabItem value="冬が来た">aaaaaaaaaaaa</TabItem>
-          <TabItem value="白い樹樹の光を">aaaaaaaaaa</TabItem>
-          <TabItem value="adfasf">aaaaa</TabItem>
-        </Tabs>
       </div>
     </Layout>
   );
