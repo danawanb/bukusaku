67785a872e70826f317a9cf6d7285623049a8d41
danawanb <danawanbimantoro23@gmail.com>
2022-06-23 17:03:19 +0700

    Create index.js

---
+++ src/pages/todo/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Hello() {
+  return (
+    <Layout title="asd" description="Hasde">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <p>
+          asdasdadsdas
+        </p>
+      </div>
+    </Layout>
+  );
+}
