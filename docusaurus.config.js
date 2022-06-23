d41b88b193ebb2550a4e9e621277a336731c16ff
danawanb <danawanbimantoro23@gmail.com>
2022-06-23 17:07:50 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ module.exports = {
           position: 'left',
         },
         {to: 'blog', label: 'Pengumuman', position: 'left'},
-        {to: 'src/pages/todo/index.js', label: 'Jadwal', position: 'left'},
+        {to: 'todo', label: 'Jadwal', position: 'left'},
       ],
     },
     footer: {
@@ -63,6 +63,9 @@ module.exports = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        todo: {
+         //////
+        }
       },
     ],
   ],
