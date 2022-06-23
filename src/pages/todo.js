8070a45abe67d3f0fd7ca11d6d899c2d8cc74a68
danawanb <danawanbimantoro23@gmail.com>
2022-06-23 22:58:28 +0700

    Update todo.js

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -7,13 +7,13 @@ export default function Jadwal() {
     <Layout title="asd" description="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa">
       <div
         style={{
-          display: 'flex',
+          //display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          justify-content:center;
+          justify-content:center,
         }}>
         <h1>01101000 01100001 01101000 01100001 01101000 01100001 01101000</h1>
-//         <img src="https://awasbucket.s3.ap-southeast-3.amazonaws.com/136192f9-4348-4110-8f28-8bb5b04e4144.png"/>
+        <img src="https://awasbucket.s3.ap-southeast-3.amazonaws.com/136192f9-4348-4110-8f28-8bb5b04e4144.png"/>
       </div>
     </Layout>
   );
