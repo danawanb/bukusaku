eeb6088a5c1d0dfabdab255d5e143bbea86a2ab5
danawanb <danawanbimantoro23@gmail.com>
2022-06-23 23:00:08 +0700

    Update todo.js

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -10,7 +10,7 @@ export default function Jadwal() {
           //display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          justify-content:center,
+          justify-content: 'center',
         }}>
         <h1>01101000 01100001 01101000 01100001 01101000 01100001 01101000</h1>
         <img src="https://awasbucket.s3.ap-southeast-3.amazonaws.com/136192f9-4348-4110-8f28-8bb5b04e4144.png"/>
