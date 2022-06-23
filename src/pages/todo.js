efe6a4c94d1fc773d823b1f2438b64072e614e1c
danawanb <danawanbimantoro23@gmail.com>
2022-06-23 22:37:40 +0700

    Update todo.js

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -3,7 +3,7 @@ import Layout from '@theme/Layout';
 
 export default function Jadwal() {
   return (
-    <Layout title="Todo" description="aaaaaaaaaaaaaaaaaaaaaaaaa">
+    <Layout title="asd" description="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa">
       <div
         style={{
           display: 'flex',
@@ -13,8 +13,28 @@ export default function Jadwal() {
           fontSize: '20px',
         }}>
         <p>
-         aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         </p>
+
+        <Tabs className="unique-tabs">
+          <TabItem value="冬が来た">aaaaaaaaaaaa</TabItem>
+          <TabItem value="白い樹樹の光を">aaaaaaaaaa</TabItem>
+          <TabItem value="adfasf">aaaaa</TabItem>
+        </Tabs>
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+        <Tabs className="unique-tabs">
+          <TabItem value="冬が来た">aaaaaaaaaaaa</TabItem>
+          <TabItem value="白い樹樹の光を">aaaaaaaaaa</TabItem>
+          <TabItem value="adfasf">aaaaa</TabItem>
+        </Tabs>
       </div>
     </Layout>
   );
