bd0262ba8f9ded8baf351fa1d8cf77b33636ed60
danawanb <danawanbimantoro23@gmail.com>
2022-06-24 15:01:48 +0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,9 @@
 module.exports = {
   tutorialSidebar: {
-    'Prinsip Dasar': ['dasar1'],
+    'Prinsip Dasar': ['dasar1', 'doc2'],
     'Unsur Pendidikan': ['doc1', 'doc2','doc3'],
     'Unsur Pengembangan Profesi': ['bangprof1', 'bangprof2','bangprof3'],
     'Unsur Koordinator/Sub Koordinator': ['doc1', 'doc2','doc3'],
   },
-   Features: ['dasar1'],
+   Features: ['doc2'],
 };
